2ced404b0b713ec5b1575262ea300cbd376c02b8
Tasso Barbosa <tassobarbosa@mgail.com>
2021-03-15 16:18:22 -0300

    Calculadora de inteiros funcionando

--- calculadora.cpp
+++ calculadora.cpp
@@ -8,6 +8,11 @@ Entry * txtResult;
 Button * btn9, * btn8,* btn7,* btn6,* btn5,* btn4,* btn3,* btn2,* btn1,* btn0;
 Button * btnDevide, * btnTimes, *btnMinus, *btnPlus, * btnDot, * btnEqual, * btnClean;
 
+double number1 = 0;
+double number2 = 0;
+double result = 0;
+int which_operation = 0;
+
 void on_btn9_clicked(){
   std::string screen = txtResult->get_text();
   screen+="9";
@@ -62,9 +67,51 @@ void on_btnClean_clicked(){
   std::string screen = txtResult->get_text();
   screen = "";
   txtResult->set_text(screen);
+  number1 = number2 = result = which_operation = 0;
 }
 
+void on_btnPlus_clicked(){
+  std::string screen_number = txtResult->get_text();
+  which_operation = 1;
+  number1 = std::stoi(screen_number);
+  screen_number="";
+  txtResult->set_text(screen_number);
+}
 
+void on_btnMinus_clicked(){
+  std::string screen_number = txtResult->get_text();
+  which_operation = 2;
+  number1 = std::stoi(screen_number);
+  screen_number="";
+  txtResult->set_text(screen_number);
+}
+void on_btnTimes_clicked(){
+  std::string screen_number = txtResult->get_text();
+  which_operation = 3;
+  number1 = std::stoi(screen_number);
+  screen_number="";
+  txtResult->set_text(screen_number);
+}
+void on_btnDevide_clicked(){
+  std::string screen_number = txtResult->get_text();
+  which_operation = 4;
+  number1 = std::stoi(screen_number);
+  screen_number="";
+  txtResult->set_text(screen_number);
+}
+void on_btnEqual_clicked(){
+  std::string screen_number = txtResult->get_text();
+
+  number2 = std::stoi(screen_number);
+  if(which_operation == 1) result = number1 + number2;
+  if(which_operation == 2) result = number1 - number2;
+  if(which_operation == 3) result = number1 * number2;
+  if(which_operation == 4) result = number1 / number2;
+
+  number1 = number2 = 0;
+
+  txtResult->set_text(std::to_string(result));
+}
 
 int main(int argc, char **argv){
 
@@ -73,7 +120,7 @@ int main(int argc, char **argv){
 
   refBuilder-> add_from_file("calculadora.glade");
   refBuilder-> get_widget("window", pWindow);
-
+//Button * btnDot;
   if(pWindow){
     refBuilder-> get_widget("btn9", btn9);
     refBuilder-> get_widget("btn8", btn8);
@@ -87,6 +134,11 @@ int main(int argc, char **argv){
     refBuilder-> get_widget("btn0", btn0);
     refBuilder-> get_widget("txtResult", txtResult);
     refBuilder-> get_widget("btnClean", btnClean);
+    refBuilder-> get_widget("btnPlus", btnPlus);
+    refBuilder-> get_widget("btnMinus", btnMinus);
+    refBuilder-> get_widget("btnTimes", btnTimes);
+    refBuilder-> get_widget("btnDevide", btnDevide);
+    refBuilder-> get_widget("btnEqual", btnEqual);
   }
 
 
@@ -123,9 +175,26 @@ int main(int argc, char **argv){
   if(btnClean){
     btnClean->signal_clicked().connect(sigc::ptr_fun(on_btnClean_clicked));
   }
-
+  if(btnPlus){
+    btnPlus->signal_clicked().connect(sigc::ptr_fun(on_btnPlus_clicked));
+  }
+  if(btnMinus){
+    btnMinus->signal_clicked().connect(sigc::ptr_fun(on_btnMinus_clicked));
+  }
+  if(btnTimes){
+    btnTimes->signal_clicked().connect(sigc::ptr_fun(on_btnTimes_clicked));
+  }
+  if(btnDevide){
+    btnDevide->signal_clicked().connect(sigc::ptr_fun(on_btnDevide_clicked));
+  }
+  if(btnEqual){
+    btnEqual->signal_clicked().connect(sigc::ptr_fun(on_btnEqual_clicked));
+  }
 
   app->run (*pWindow);
   delete pWindow;
   return 0;
 }
+
+
+//g++ calculadora.cpp -o calculadora $(pkg-config gtkmm-3.0 --cflags --libs) -export-dynamic
